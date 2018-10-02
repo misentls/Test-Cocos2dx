7894b2de1195fd110d38294febdd49351d185549
Mã Văn Tài <misennguyen95@gmail.com>
2018-10-03 01:06:08 +0700

    Add files via upload

---
+++ CMakeFiles/MyGame.dir/DependInfo.cmake
@@ -0,0 +1,41 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/misenn/work/Cocos/Test/Classes/AppDelegate.cpp" "/home/misenn/work/Cocos/Test/CMakeFiles/MyGame.dir/Classes/AppDelegate.cpp.o"
+  "/home/misenn/work/Cocos/Test/Classes/HelloWorldScene.cpp" "/home/misenn/work/Cocos/Test/CMakeFiles/MyGame.dir/Classes/HelloWorldScene.cpp.o"
+  "/home/misenn/work/Cocos/Test/proj.linux/main.cpp" "/home/misenn/work/Cocos/Test/CMakeFiles/MyGame.dir/proj.linux/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "LINUX"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include/GLFW"
+  "/usr/include/GLFW"
+  "cocos2d"
+  "cocos2d/cocos"
+  "cocos2d/external"
+  "cocos2d/cocos/platform"
+  "cocos2d/cocos/audio/include"
+  "Classes"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/misenn/work/Cocos/Test/cocos2d/cocos/CMakeFiles/cocos2d.dir/DependInfo.cmake"
+  "/home/misenn/work/Cocos/Test/cocos2d/cocos/CMakeFiles/cocos2dInternal.dir/DependInfo.cmake"
+  "/home/misenn/work/Cocos/Test/cocos2d/external/unzip/CMakeFiles/unzip.dir/DependInfo.cmake"
+  "/home/misenn/work/Cocos/Test/cocos2d/external/tinyxml2/CMakeFiles/tinyxml2.dir/DependInfo.cmake"
+  "/home/misenn/work/Cocos/Test/cocos2d/external/xxhash/CMakeFiles/xxhash.dir/DependInfo.cmake"
+  "/home/misenn/work/Cocos/Test/cocos2d/external/recast/CMakeFiles/recast.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
